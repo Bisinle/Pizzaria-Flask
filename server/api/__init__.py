7a17c8273830406fecfa8bb7869901a79ae6ec60
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-09-26 00:34:05 +0300

    C O R S added

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -4,10 +4,12 @@ from flask import Flask, make_response,jsonify
 from flask_migrate import Migrate
 from flask_restful import Resource, Api
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 
 # create both app and api instances
 app = Flask(__name__)
+CORS(app)
 api = Api(app)
 
 
