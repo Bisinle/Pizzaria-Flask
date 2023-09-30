dfcf35d85d7125d8b7162c7be4b063198c93673e
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-09-30 14:01:25 +0300

    documented the api with flask-restX

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ function App() {
       .then((res) => res.json())
       .then((data) => setData(data));
   }, []);
-  console.log(data);
+  // console.log(data);
   return (
     <div className="App">
       <Navbar />
